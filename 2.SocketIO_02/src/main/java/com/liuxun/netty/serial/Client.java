91ce79a1552f4a451297134cedbb1a4dbdb0e8c6
LX1993728 <2652790899@qq.com>
2018-09-28 20:37:33 +0800

    +++++++使用JBoss Marshalling序列化机制 为Netty序列化传输对象++++++++

--- 2.SocketIO_02/src/main/java/com/liuxun/netty/serial/Client.java
+++ 2.SocketIO_02/src/main/java/com/liuxun/netty/serial/Client.java
@@ -1,5 +1,6 @@
 package com.liuxun.netty.serial;
 
+import com.liuxun.utils.GzipUtils;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -7,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,18 +20,19 @@ public class Client {
         Bootstrap b = new Bootstrap();
         b.group(group)
                 .channel(NioSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
                 .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel sc) throws Exception {
                         sc.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingDecoder());
                         sc.pipeline().addLast(MarshallingCodeCFactory.buildMarshallingEncoder());
-                        // TODO: add custom handler
+                        sc.pipeline().addLast(new ClientHandler());
                     }
                 });
 
-        ChannelFuture cf = b.connect("127.0.0.1", 8765);
+        ChannelFuture cf = b.connect("127.0.0.1", 8765).sync();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i <= 6; i++) {
             Req req = new Req();
             req.setId("" + i);
             req.setName("pro" + i);
@@ -39,8 +43,11 @@ public class Client {
             byte[] data = new byte[in.available()];
             in.read(data);
             in.close();
-//            req.setAttachment(Gzip);
+            req.setAttachment(GzipUtils.gzip(data));
+            cf.channel().writeAndFlush(req);
         }
 
+        cf.channel().closeFuture().sync();
+        group.shutdownGracefully();
     }
 }
