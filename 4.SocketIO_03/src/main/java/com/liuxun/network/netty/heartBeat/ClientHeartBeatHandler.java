6d57c59b2044f39296be637672912778d5ac4233
LX1993728 <2652790899@qq.com>
2018-10-16 12:46:28 +0800

    +++++complete HeartBeat  +++

--- 4.SocketIO_03/src/main/java/com/liuxun/network/netty/heartBeat/ClientHeartBeatHandler.java
+++ 4.SocketIO_03/src/main/java/com/liuxun/network/netty/heartBeat/ClientHeartBeatHandler.java
@@ -2,11 +2,17 @@ package com.liuxun.network.netty.heartBeat;
 
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+import org.hyperic.sigar.CpuPerc;
+import org.hyperic.sigar.Mem;
+import org.hyperic.sigar.Sigar;
 
 import java.net.InetAddress;
+import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @apiNote
@@ -32,6 +38,61 @@ public class ClientHeartBeatHandler extends ChannelHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx, msg);
+        System.out.println("client: "+msg);
+        try{
+            if (msg instanceof String){
+                String ret = (String) msg;
+                if (SUCCESS_KEY.equals(ret)){
+                    // 握手成功，主动发送心跳信息
+                    this.heartBeat = this.scheduler.scheduleWithFixedDelay(new HeartBeatTask(ctx),0,2, TimeUnit.SECONDS);
+                }else {
+                    System.out.println(msg);
+                }
+            }
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
+
+
+    private class HeartBeatTask implements Runnable{
+        private final ChannelHandlerContext ctx;
+
+        public HeartBeatTask(ChannelHandlerContext ctx) {
+            this.ctx = ctx;
+        }
+
+        @Override
+        public void run() {
+
+            try{
+                RequestInfo info = new RequestInfo();
+                // ip
+                info.setIp(addr.getHostAddress());
+                Sigar sigar = new Sigar();
+                // CPU prec
+                CpuPerc cpuPerc = sigar.getCpuPerc();
+                HashMap<String,Object> cpuPercMap = new HashMap<>();
+                cpuPercMap.put("combined", cpuPerc.getCombined());
+                cpuPercMap.put("user",cpuPerc.getUser());
+                cpuPercMap.put("sys",cpuPerc.getSys());
+                cpuPercMap.put("wait",cpuPerc.getWait());
+                cpuPercMap.put("idle",cpuPerc.getIdle());
+
+                // memory
+                Mem mem = sigar.getMem();
+                HashMap<String,Object> memoryMap = new HashMap<>();
+                memoryMap.put("total",mem.getTotal()/1024L);
+                memoryMap.put("used",mem.getUsed()/1024L);
+                memoryMap.put("free",mem.getFree()/1024L);
+                info.setCpuPercMap(cpuPercMap);
+                info.setMemoryMap(memoryMap);
+                ctx.writeAndFlush(info);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
